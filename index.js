479015990bbd75701a0b0af82c4fa97bac3b02d1
Rouxy_senpai <52127278+Pandaroux1400@users.noreply.github.com>
2021-04-27 17:05:58 +0200

    1.11
    rectification de bug et EasterEeg

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const test = document.getElementById('test')
-console.log(test);
+//console.log(test);
 
 test.addEventListener('dblclick', () => {
 
-	alert("https://www.youtube.com/watch?v=T-YQrScC-F4");
+	var output = document.getElementById('output')
+	console.log(test);
+	output.innerHTML = `<a href="https://www.youtube.com/watch?v=T-YQrScC-F4" target="_blank">Bravo!</a>`;
 
 })
